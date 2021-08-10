93ce37fa7fe52ab0af6c7a3516688f8b556bc0a2
coolperso <86847699+coolperso@users.noreply.github.com>
2021-08-10 11:38:19 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# zombie-crusher
+# zombie-crush-boilerplate
+boilerplate for crush the zombie game
---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+  Matter.Composite.add(bridge.Body,jointPoint)
+  jointLink=new Link(bridge,jointPoint)
+
+  for(var i=0;i<=8;i++) {
+    var x= random(width/2-200,width/2+300);
+    var y =random(-10,140)
+    var stone=new Stone(x,y,80,80);
+    stones.push(stone)
+  }
+}
